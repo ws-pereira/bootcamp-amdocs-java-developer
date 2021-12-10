207f844b0df66d6677d7133e285ab186521420e0
Wanderson de Souza Pereira <66091904+ws-pereira@users.noreply.github.com>
2021-12-10 12:26:28 -0300

    Exercício com operadores Relacionais em Java

---
+++ Exerc-Operadores-Relacionais.java
@@ -0,0 +1,57 @@
+public class MyClass {
+    public static void main(String args[]) {
+      
+      // Declaração dos tipos de variáveis e atribuindo seus respectivos valores
+      int i1=10, i2=20;
+      float f1=4.10f, f2=3.5f;
+      char c1='x', c2='y';
+      String s1 = "Fulando de Tal", s2 = "Fulano";
+      boolean b1 = true, b2 = false;
+      
+      // Tipos adicionas com seus respecitvos valores, mas não comparados
+      double d1 = 59.60d;
+      long l1 = 1597L, l2 = 8997L;
+      byte by = 1;
+      short sh = 25;
+      
+      
+      
+      //Apresentando o resultado das comparações
+      System.out.println("Valores i1 = " + i1 + " - i2 = " + i2 +"\n");
+      System.out.println("i1 == i2 " +  (i1 == i2));
+      System.out.println("i1 != i2 " +  (i1 != i2));
+      System.out.println("i1  > i2 " +  (i1  > i2));
+      System.out.println("i1 >= i2 " +  (i1 >= i2));
+      System.out.println("\nValores f1 = " + f1 + " - f2 = " + f2 +"\n");
+      System.out.println("f1 == f2 " +  (f1 == f2));
+      System.out.println("f1 != f2 " +  (f1 != f2));
+      System.out.println("f1  > f2 " +  (f1  > f2));
+      System.out.println("f1 >= f2 " +  (f1 >= f2));
+      System.out.println("\nValores c1 = " + c1 + " - c2 = " + c2 +"\n");
+      System.out.println("c1 == c2 " +  (c1 == c2));
+      System.out.println("c1 != c2 " +  (c1 != c2));
+      System.out.println("c1  > c2 " +  (c1  > c2));
+      System.out.println("c1 >= c2 " +  (c1 >= c2));
+       System.out.println("\nValores s1 = " + s1 + " - s2 = " + s2 +"\n");
+      System.out.println("s1 == s2 " +  (s1 == s2));
+      System.out.println("s1 != s2 " +  (s1 != s2));
+     /* 
+      As linhas abaixo geran um erro
+      System.out.println("s1  > s2 " +  (s1  > s2));
+      System.out.println("s1 >= s2 " +  (s1 >= s2));*/
+       System.out.println("\nValores b1 = " + b1 + " - b2 = " + b2 +"\n");
+      System.out.println("b1 == b2 " +  (b1 == b2));
+      System.out.println("b1 != b2 " +  (b1 != b2));
+     /*
+      As linhas abaixo geran um erro
+      System.out.println("b1  > b2 " +  (b1  > b2));
+      System.out.println("b1 >= b2 " +  (b1 >= b2));*/
+      
+       System.out.println("\nValores c1 = " + c1 + " - c2 = " + c2 +"\n");
+      System.out.println("c1 == c2 " +  (c1 == c2));
+      System.out.println("c1 != c2 " +  (c1 != c2));
+      System.out.println("c1  > c2 " +  (c1  > c2));
+      System.out.println("c1 >= c2 " +  (c1 >= c2));
+      
+    }
+}
