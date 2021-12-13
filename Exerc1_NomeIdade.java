8a8ac7d228aee101fef2761853548904e71a66b4
Wanderson de Souza Pereira <66091904+ws-pereira@users.noreply.github.com>
2021-12-13 13:09:23 -0300

    Adicionando loop com condição de saída

--- Exerc1_NomeIdade.java
+++ Exerc1_NomeIdade.java
@@ -9,18 +9,23 @@ public class Exerc1_NomeIdade {
     public static void main(String args[]) {
         
         Scanner sc = new Scanner(System.in);
-        
-        System.out.println("What's your name? ");
-        String name  = sc.next();
-        System.out.println("How old are you? ");
-        int yearsOld = sc.nextInt();
-        
-        
-        Calendar cal = Calendar.getInstance();
+         Calendar cal = Calendar.getInstance();
         int month = cal.get(Calendar.YEAR);
-       
-        int yearBorn = month - yearsOld;
-        System.out.println("Your born in " + yearBorn);
-     
+        
+        int ctrl; // Variável de controle do laço
+        
+        do{
+        
+            System.out.println("What's your name? ");
+            String name  = sc.next();
+            System.out.println("How old are you? ");
+            int yearsOld = sc.nextInt();
+        
+            int yearBorn = month - yearsOld;
+            System.out.println("Your born in " + yearBorn);
+            System.out.println("Type number zero to exit - ");
+            ctrl = sc.nextInt();;
+        }while(ctrl != 0);
+        System.out.println("Exit to loop!!");
     }
 }
